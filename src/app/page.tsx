414c99c6722826cc91c565cdb954cd3065430661
shubhsardana29 <shubhsardana29@gmail.com>
2023-12-09 01:42:37 +0530

    Refactor page.tsx to improve readability and accessibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,8 @@ export default function Home() {
       <MaxWidthWrapper>
         <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            <span className="text-rose-600">Saksham</span> Empowering People with Disabilities
+            <span className="text-rose-600">Saksham</span> Empowering People
+            with Disabilities
           </h1>
           <p className="mt-6 text-lg max-w-prose text-muted-foreground">
             Something nice and catchy get the attention of the user something
@@ -77,12 +78,11 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="mx-auto max-w-6xl px-6 lg:px-8 mt-8 text-center">
+      <div className="mx-auto max-w-6xl px-6 lg:px-8 mt-8 text-center py-5">
         <div className="flex flex-col-reverse sm:flex-row items-center">
-          
           <div className="flex-shrink-0 w-full sm:w-1/2 mb-4 sm:mb-0">
             <Image
-              src="/hero.png" 
+              src="/hero.png"
               alt="Description of the image"
               width={400}
               height={200}
@@ -92,9 +92,82 @@ export default function Home() {
           </div>
 
           <div className="w-full sm:w-1/2">
-            <h2 className="text-3xl font-bold mb-4">Breaking Barriers, Building Opportunities</h2>
-            <p className="text-lg text-gray-700">
-            Saksham: Empowering Specially Abled Individuals is a groundbreaking initiative that aims to create a more inclusive society in India. We believe that everyone, regardless of their abilities, should have the opportunity to learn, communicate, and thrive. Through our programs and services, we provide support, resources, and empowerment to individuals with disabilities, enabling them to lead fulfilling lives.
+            <h2 className="text-3xl font-bold mb-4 text-right">
+              Breaking Barriers, Building Opportunities
+            </h2>
+            <p className="text-lg text-gray-700 text-right">
+              Saksham: Empowering Specially Abled Individuals is a
+              groundbreaking initiative that aims to create a more inclusive
+              society in India. We believe that everyone, regardless of their
+              abilities, should have the opportunity to learn, communicate, and
+              thrive. Through our programs and services, we provide support,
+              resources, and empowerment to individuals with disabilities,
+              enabling them to lead fulfilling lives.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="mx-auto max-w-6xl px-6 lg:px-8 mt-8 text-center">
+        <h2 className="text-3xl font-bold text-center mb-4 py-8">
+          Our Mission & Vision
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          
+          <h2 className="text-2xl font-bold mb-4 text-left">
+            Empowering Specially Abled Individuals with Accessible Education,
+            Community Support, and Skill Development
+          </h2>
+         
+            <p className="text-sm text-gray-500 text-left">
+              At Saksham, we are dedicated to building a society that is more
+              inclusive by offering accessible education, community support, and
+              skill development opportunities to people with disabilities. Our
+              goal is to enable individuals of varying abilities to acquire
+              knowledge, communicate effectively, and succeed.
+            </p>
+         
+        </div>
+
+       
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 py-10">
+          <div>
+            <p className="text-lg font-bold mb-2 text-left">
+              Accessible Education for All
+            </p>
+            <p className="text-gray-700 text-left">
+              Our accessible education program ensures that individuals with
+              disabilities have equal access to quality education, enabling them
+              to reach their full potential.
+            </p>
+            <p className="text-rose-600 font-bold mt-2 cursor-pointer text-left">
+              Learn More{" "}
+            </p>
+          </div>
+          <div>
+            <p className="text-lg font-bold mb-2 text-left">
+              Community Support for Inclusion
+            </p>
+            <p className="text-gray-700 text-left">
+              Through our community support initiatives, we foster an inclusive
+              environment where individuals with disabilities feel supported,
+              valued, and connected.
+            </p>
+            <p className="text-rose-600 font-bold mt-2 cursor-pointer text-left">
+              Learn More{" "}
+            </p>
+          </div>
+          <div>
+            <p className="text-lg font-bold mb-2 text-left">
+              Skill Development for Empowerment
+            </p>
+            <p className="text-gray-700 text-left">
+              Our skill development programs equip specially abled individuals
+              with the necessary skills and knowledge to enhance their
+              employability and independence.
+            </p>
+            <p className="text-rose-600 font-bold mt-2 cursor-pointer text-left">
+              Learn More{" "}
             </p>
           </div>
         </div>
@@ -108,7 +181,7 @@ export default function Home() {
         <Accordion type="single" collapsible>
           <AccordionItem value="item-1">
             <AccordionTrigger>What is Saksham?</AccordionTrigger>
-            <AccordionContent>
+            <AccordionContent className="text-left">
               Saksham: Empowering Specially Abled Individuals is a
               groundbreaking initiative aimed at creating a more inclusive
               society in India where everyone, regardless of their abilities,
@@ -117,7 +190,7 @@ export default function Home() {
           </AccordionItem>
           <AccordionItem value="item-2">
             <AccordionTrigger>How can I contribute?</AccordionTrigger>
-            <AccordionContent>
+            <AccordionContent className="text-left">
               There are many ways you can contribute to Saksham: Empowering
               Specially Abled Individuals. You can volunteer your time, donate
               funds, or spread awareness about the initiative.
@@ -125,7 +198,7 @@ export default function Home() {
           </AccordionItem>
           <AccordionItem value="item-3">
             <AccordionTrigger>Who can benefit from Saksham?</AccordionTrigger>
-            <AccordionContent>
+            <AccordionContent className="text-left">
               Saksham: Empowering Specially Abled Individuals is designed to
               benefit individuals with disabilities of all kinds. Our programs
               and resources are tailored to meet the unique needs of each
@@ -134,7 +207,7 @@ export default function Home() {
           </AccordionItem>
           <AccordionItem value="item-4">
             <AccordionTrigger>How can i get involved?</AccordionTrigger>
-            <AccordionContent>
+            <AccordionContent className="text-left">
               Getting involved with Saksham: Empowering Specially Abled
               Individuals is easy. You can join our volunteer network,
               participate in fundraising events, or become a corporate partner.
@@ -142,7 +215,7 @@ export default function Home() {
           </AccordionItem>
           <AccordionItem value="item-5">
             <AccordionTrigger>Where is Saksham based?</AccordionTrigger>
-            <AccordionContent>
+            <AccordionContent className="text-left">
               Saksham: Empowering Specially Abled Individuals is based in Delhi,
               but our reach extends to individuals and communities across the
               country.
