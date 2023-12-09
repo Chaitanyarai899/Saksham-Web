390588fc602360e9a87be86045d41d9e99234b1c
Aditya <adityasinghboss1234@gmail.com>
2023-12-09 21:36:25 +0530

    auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { auth, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
-import Image from "next/image";
-import { Button } from "@/components/ui/button"
-import { cn } from "@/lib/utils";
-import { auth, UserButton } from "@clerk/nextjs";
-
 
 const Navbar = () => {
   const { userId } = auth();
-
 
   return (
     <nav
@@ -18,34 +15,40 @@ const Navbar = () => {
       <MaxWidthWrapper>
         <div className="flex h-16 items-center justify-between pb-2">
           <Link href="/" className="flex z-40 ">
-           <Image
-                    src="/sakshamlogo1.png"
-                    alt="Preview of video call feature"
-                    width={200}
-                    height={40}
-                    quality={100}
-                    className="rounded-md"
-                  />
+            <Image
+              src="/sakshamlogo1.png"
+              alt="Preview of video call feature"
+              width={200}
+              height={40}
+              quality={100}
+              className="rounded-md"
+            />
           </Link>
-        <div className="space-x-4" id="auth-box">
-          {!userId && (
-            <>
-              <Link href="/sign-in" className="font-mono">
-                <Button variant="ghost" className="border-rose-600 border-b-[4px] border-r-[4px] border-l-[2px] border-t-[2px] m-2 hover:border-r-[3px] hover:border-b-[3px] hover:bg-white">
-                  Sign In
-                </Button>
-              </Link>
-              <Link href="/sign-up" className="font-mono">
-                <Button variant="outline" className="border-rose-600 border-b-[4px] border-r-[4px] border-l-[2px] border-t-[2px] m-2 hover:border-r-[3px] hover:border-b-[3px hover:bg-white">
-                  Sign Up
-                </Button>
-              </Link>
-            </>
-          )}
-          <div className="ml-auto">
-            <UserButton afterSignOutUrl="/" />
+          <div className="space-x-4" id="auth-box">
+            {!userId && (
+              <>
+                <Link href="/sign-in" className="font-mono">
+                  <Button
+                    variant="ghost"
+                    className="border-rose-600 border-b-[4px] border-r-[4px] border-l-[2px] border-t-[2px] m-2 hover:border-r-[3px] hover:border-b-[3px] hover:bg-white"
+                  >
+                    Sign In
+                  </Button>
+                </Link>
+                <Link href="/sign-up" className="font-mono">
+                  <Button
+                    variant="outline"
+                    className="border-rose-600 border-b-[4px] border-r-[4px] border-l-[2px] border-t-[2px] m-2 hover:border-r-[3px] hover:border-b-[3px hover:bg-white"
+                  >
+                    Sign Up
+                  </Button>
+                </Link>
+              </>
+            )}
+            <div className="ml-auto">
+              <UserButton afterSignOutUrl="/" />
+            </div>
           </div>
-        </div>
         </div>
       </MaxWidthWrapper>
     </nav>
