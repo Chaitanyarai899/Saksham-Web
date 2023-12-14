0a03de47d5675182323d7c0afed0645144d73ace
Aditya <adityasinghboss1234@gmail.com>
2023-12-14 06:43:28 +0530

    create & join meet rooms

--- src/app/room/[...slug]/page.tsx
+++ src/app/room/[...slug]/page.tsx
@@ -1,5 +1,20 @@
-function page() {
-  return <div>room id</div>;
+"use client";
+import { ws } from "@/lib/ws";
+import React, { useEffect } from "react";
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+function page({ params: { slug } }: Props) {
+  const id = slug;
+
+  useEffect(() => {
+    ws.emit("join-room", { roomId: id });
+  }, [id]);
+  return <div>Room Id: {id}</div>;
 }
 
 export default page;
