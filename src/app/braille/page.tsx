0f03b4c7166ad5204c521e0a083baa931d7b2acd
Ayush <ayush4002gupta@gmail.com>
2023-12-16 17:22:41 +0530

    orbit writer

---
+++ src/app/braille/page.tsx
@@ -0,0 +1,67 @@
+"use client" 
+import React, { useState, ChangeEvent, KeyboardEvent } from 'react';
+
+function Braille() {
+  const brailleMap: Record<string, string> = {
+    S: "A",
+    SD: "B",
+    SK: "C",
+    SJK: "D",
+    SJD: "F",
+    SJKK: "G",
+    SKJ: "H",
+    DJ: "I",
+    DFK: "J",
+    SDJ: "L",
+    SDF: "M",
+    SDFK: "N",
+    SDK: "O",
+    SDFJ: "P",
+    SDFKJ: "Q",
+    DF: "S",
+    DFDK: "T",
+    SL: "U",
+    SDFL: "V",
+    SDJKL: "W",
+    SDFKL: "X",
+    SDFKK: "Y",
+    SDKK: "Z",
+  };
+  const [inputValue, setInputValue] = useState('');
+  const [outputValue, setOutputValue] = useState('');
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const input = event.target.value.toUpperCase();
+    setInputValue(input);
+  };
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      if (brailleMap[inputValue]) {
+        setOutputValue((prevValue) => prevValue + brailleMap[inputValue]);
+        setInputValue('');
+      }
+    } else if (event.key === ' ') {
+      event.preventDefault();
+      setOutputValue((prevValue) => prevValue.trim() + ' ');
+    }
+  };
+
+  return (
+    <div>
+      <label htmlFor="brailleInput">Enter Braille Key:</label>
+      <input
+        id="brailleInput"
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+      />
+      {outputValue && (
+        <p>Continuous Word: {outputValue}</p>
+      )}
+    </div>
+  );
+}
+
+export default Braille;
