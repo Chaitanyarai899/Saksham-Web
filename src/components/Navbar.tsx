1141c4a838bb649895053b6d0b89ce8ee2ac39a0
Vansh Goel <goelvansh82@gmail.com>
2023-12-18 10:20:21 +0530

    fixed nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,8 +25,8 @@ const Navbar = () => {
       <div className="vmd:hidden fixed left-2 top-3" >
           <DropdownMenu>
             <DropdownMenuTrigger><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 24 24">
-<path d="M5,7h2h12c1.1,0,2-0.9,2-2s-0.9-2-2-2H7H5C3.9,3,3,3.9,3,5S3.9,7,5,7z"></path><path d="M19,10h-3H5c-1.1,0-2,0.9-2,2s0.9,2,2,2h11h3c1.1,0,2-0.9,2-2S20.1,10,19,10z"></path><path d="M19,17h-6H5c-1.1,0-2,0.9-2,2s0.9,2,2,2h8h6c1.1,0,2-0.9,2-2S20.1,17,19,17z"></path>
-</svg></DropdownMenuTrigger>
+            <path d="M5,7h2h12c1.1,0,2-0.9,2-2s-0.9-2-2-2H7H5C3.9,3,3,3.9,3,5S3.9,7,5,7z"></path><path d="M19,10h-3H5c-1.1,0-2,0.9-2,2s0.9,2,2,2h11h3c1.1,0,2-0.9,2-2S20.1,10,19,10z"></path><path d="M19,17h-6H5c-1.1,0-2,0.9-2,2s0.9,2,2,2h8h6c1.1,0,2-0.9,2-2S20.1,17,19,17z"></path>
+            </svg></DropdownMenuTrigger>
             <DropdownMenuContent>
               <DropdownMenuLabel>Hello There !</DropdownMenuLabel>
               <DropdownMenuSeparator />
@@ -36,13 +36,10 @@ const Navbar = () => {
               <DropdownMenuItem>main</DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
-          '
         </div>
-        '
-git 
-        <div className="flex h-16 items-center justify-between pb-2">
-        
-          <Link href="/" className="flex z-40 ">
+
+        <div className="flex items-center justify-between h-16  pb-2">
+          <Link href="/" className="flex z-40">
             <Image
               src="/sakshamlogo1.png"
               alt="Preview of video call feature"
@@ -52,7 +49,7 @@ git
               className="rounded-md items-center "
             />
           </Link>
-          <div className="space-x-4" id="auth-box">
+          <div className="space-x-4 top-1 mr-2 fixed right-5" id="auth-box">
             {!userId && (
               <>
                 <Link href="/sign-in" className="font-mono">
@@ -73,14 +70,14 @@ git
                 </Link>
               </>
             )}
-            <div className="flex flex-row ">
+            <div className="flex-row">
               {userId && (
-                <div className="pt-1 md:hidden pl-[17vw] xlg:pl-[13vw] xmd:pl-[7vw]">
+                <div className="pt-1 pr-[23vw]">
                   <MainNav />
                 </div>
               )}
 
-              <div className=" top-3 mr-2  fixed right-5">
+              <div className=" top-3 mr-2 fixed right-5">
                 <UserButton afterSignOutUrl="/" />
               </div>
             </div>
